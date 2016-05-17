31f53a08a28d7e221114338e2867128f0a5947e9
Lee Johnson <lee@givengain.ch>
2016-05-17 08:17:28 +0200

    bump VERSION and Changes for CPAN release

--- lib/Swagger2/Markdown.pm
+++ lib/Swagger2/Markdown.pm
@@ -10,7 +10,7 @@ Swagger2::Markdown - convert a Swagger2 spec to various markdown formats
 
 =head1 VERSION
 
-0.06
+0.07
 
 =head1 SYNOPSIS
 
@@ -45,7 +45,7 @@ use Template;
 use Swagger2::Markdown::API::Blueprint;
 use Pod::Markdown;
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 =head1 ATTRIBUTES
 
